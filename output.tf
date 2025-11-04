9ed29a18c4a5938c46b6f0bd335659e78b09c570
ketan <ketan@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-04 11:50:32 +0000

    update terraform

---
+++ output.tf
@@ -0,0 +1,16 @@
+# public ip of my ec2 instance
+
+output "my-ec2-pb-ip" {
+  value = aws_instance.example.public_ip
+}
+
+# instance id of my ec2
+output "my-ec2-instance-id" {
+    value = aws_instance.example.id
+  
+}
+# storig data in local system
+resource "local_file" "ketan-data" {
+  content  = aws_instance.example.public_ip
+  filename = "${path.module}/myip.txt"
+}
