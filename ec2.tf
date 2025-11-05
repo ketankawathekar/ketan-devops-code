1a93bdb51aabfba51a69af2dd4b03c7ea50ac53c
ketan <ketan@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-05 07:26:41 +0000

    implement var

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,12 @@
 # creating Ec2
 
 resource "aws_instance" "example" {
-    ami = "ami-0a25a306450a2cba3"
-    instance_type = "t2.nano"
-    key_name = "roche-key"
+    #ami = "ami-0a25a306450a2cba3"
+    ami = var.ketan-ami-id
+    instance_type = var.vm-size
+    key_name = var.ec2-key-name
     tags = {
-      "Name" = "ketan-vm" 
+      "Name" = var.vm-name 
     }
 
 }
---
+++ my-varvalues.tfvars
@@ -0,0 +1,4 @@
+vm-name = "ketan-vmnew-1"
+vm-size = "t2.nano"
+ec2-key-name = "roche-key"
+ketan-ami-id = "ami-0a25a306450a2cba3"
